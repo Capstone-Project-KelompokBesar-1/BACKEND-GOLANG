963573918b59c97e7de16f4029c8a687931ca2de
unknown <andianugrahaqsa@gmail.com>
2022-11-22 22:29:39 +0800

    set config

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"ourgym/config"
 	"ourgym/routes"
 )
 
 func main() {
+	config.InitConfig()
+
 	route := routes.InitRoute()
+
 	route.Logger.Fatal(route.Start(":8080"))
 }
